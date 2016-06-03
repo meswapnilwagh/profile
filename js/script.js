672e2517880ef4a35a3e0c7fd207fbecf602460b
Swapnil Wagh <djj2340@varian.com>
2016-06-03 17:05:13 +0530

    new profile site

--- js/script.js
+++ js/script.js
@@ -1,258 +1,298 @@
- var scrollOffset=-30;
+/*
+--------------------------------------------------------
 
-(function($) {
-  "use strict";
+[ INSTA , Javscript ]
 
-
-  $(window).bind("load", function() {
-    $('#status').fadeOut(); // will first fade out the loading animation
-    $('#preloader').delay(1000).fadeOut('slow'); // will fade out the white DIV that covers the website.
-    $('body').delay(1000).css({'overflow-x': 'hidden'}).css({'overflow-y': 'auto'});
-    checkContactForm();
-     $('body').scrollspy({ target: '.nav-menu' });
-
-   
-$('body').data()['bs.scrollspy'].options.offset = Math.abs(scrollOffset)+10; // Set the new offset 
-$('body').data()['bs.scrollspy'].process(); // Force scrollspy to recalculate the offsets to your targets 
-$('body').scrollspy('refresh'); // Refresh the scrollspy.
-
-
-  });
-
-
-  $(window).ready(function() {
-
-    var wow = new WOW(
-            {
-              boxClass: 'wow', // animated element css class (default is wow)
-              animateClass: 'animated', // animation css class (default is animated)
-              offset: '-200px', // distance to the element when triggering the animation (default is 0)
-              mobile: true, // trigger animations on mobile devices (default is true)
-              live: true        // act on asynchronously loaded content (default is true)
-            }
-    );
-
-    new WOW().init();
-
-    $('.skill').circliful({
-      fgcolor: "#E64A3B",
-      fontsize: "20",
-      bgcolor: "#D8CDBB",
-      dimension: "150"
-
-    });
-
-
-    $('.has-shadow').append('<div class="shadow"></div>');
-
-    function getImgSize(el, imgSrc) {
-      var newImg = new Image();
-      newImg.onload = function() {
-        var height = newImg.height;
-        var width = newImg.width;
-
-        el.css('height', height);
-
-      };
-      newImg.src = imgSrc;
-    }
-
-    if ($('.bg-image[data-bg-image]').length > 0) {
-      $('.bg-image[data-bg-image]').each(function() {
-        var el = $(this);
-        var sz = getImgSize(el, el.attr("data-bg-image"));
-        el.css('background-position', 'center').css('background-image', "url('" + el.attr("data-bg-image") + "')").css('background-size', 'cover').css('background-repeat', 'no-repeat');
-      });
-    }
-
-    if ($('.bg-color[data-bg-color]').length > 0) {
-      $('.bg-color[data-bg-color]').each(function() {
-        var el = $(this);
-        el.css('background-color', el.attr("data-bg-color"));
-      });
-    }
-
-    if ($('.portfolio-item').length > 0) {
-      var $container = $('#portfolio-grid');
-      $container.isotope({filter: '*'});
-      $('.group-selectors a').click(function(e) {
-        e.preventDefault();
-        var selector = $(this).attr('data-filter');
-        $container.isotope({filter: selector, columnWidth: 4});
-        $('.group-selectors a.active').removeClass('active');
-        $(this).toggleClass('active');
-        return false;
-      });
-      $('.group-selectors a').each(function() {
-        $(this).append('<span></span>');
-      });
-
-
-    }
-
-
-
-    $('[data-placeholder]').focus(function() {
-      var input = $(this);
-      if (input.val() == input.attr('data-placeholder')) {
-        input.val('');
-      }
-    }).blur(function() {
-      var input = $(this);
-      if (input.val() == '' || input.val() == input.attr('data-placeholder')) {
-        input.addClass('placeholder');
-        input.val(input.attr('data-placeholder'));
-      }
-    }).blur();
-
-    $('[data-placeholder]').parents('form').submit(function() {
-      $(this).find('[data-placeholder]').each(function() {
-        var input = $(this);
-        if (input.val() == input.attr('data-placeholder')) {
-          input.val('');
-        }
-      });
-    });
-    
-    
-    
-  });
-
-
-  $('.goto-top').click(function(e) {
-    e.preventDefault();
-    $('html,body').animate({
-      scrollTop: 0
-    }, 2000);
-  });
-
-  if ($('a[data-rel="prettyphoto"]').length > 0) {
-    $('a[data-rel="prettyphoto"]').prettyPhoto();
-  }
-  if ($('a[data-rel="prettyPhoto"]').length > 0) {
-    $('a[data-rel="prettyPhoto"]').prettyPhoto();
-  }
-
-
-  //Contact form setup
-  function checkContactForm() {
-    if ($(".contact-form").length > 0) {
-      var formStatus = $(".contact-form").validate();
-      //   ===================================================== 
-      //sending contact form
-      $(".contact-form").submit(function(e) {
-        e.preventDefault();
-
-        //  triggers contact form validation
-        if (formStatus.errorList.length === 0)
-        {
-          $(".contact-form .submit").fadeOut(function() {
-            $('#loading').css('visibility', 'visible');
-            $.post('submit.php', $(".contact-form").serialize(),
-                    function(data) {
-                      $(".contact-form input,.contact-form textarea").not('.submit').val('');
-                      $('.message-box').html(data);
-                      $('#loading').css('visibility', 'hidden');
-                      $(".contact-form").css('display', 'none');
-                      //$(".contact-form .submit").removeClass('disabled').css('display', 'block');
-                    }
-            );
-          });
-        }
-      });
-    }
-  }
- 
+  Template Name : INSTA - Responsive Onepage Resume Template
   
+  Version       :  1.0 
+  
+  Author        :  Pixel_Factory
+  
+  Author URI    :  http://themeforest.net/user/Pixel_Factory
+  
+  Author Email  : h.nafees.anwar@gmail.com
 
+--------------------------------------------------------
+*/
 
-//hashtag navigation address setup (deeplink)
-  $('.nav-menu a').address($(this).attr('href'));
-  $('.top-drop-menu').change(function() {
-    var loc = ($(this).find('option:selected').val());
-    $('.nav-menu a').address(loc);
+/*  ------------------
+    Remove Preloader
+    ------------------  */
 
-  });
-
-
-  $.address.change(function(event) {
-    var pageID = event.value.split('/')[1];
-    if (pageID != '' && pageID.indexOf('.') === -1) {
-      var el = $(".nav-menu [href=#" + pageID + "]");
-      $('.nav-menu .active').removeClass('active');
-      el.parent().addClass('active');
-      $('select.nav option').each(function() {
-        var val = $(this).val();
-        if (val === "#" + pageID) {
-          $('select.nav option:selected').removeAttr('selected');
-          $(this).attr('selected', 'selected');
-        }
-      });
-      scrollToSection("#" + pageID);
-    } else {
-      if (pageID.indexOf('.') > -1) {
-        window.location = pageID;
-      }
-    }
-  });
-
-  $('select.nav').change(function() {
-    var loc = ($(this).find('option:selected').val());
-    scrollToSection(loc);
-  });
-
-  function scrollToSection(destSection) {
-
-    $('html, body').stop().animate({
-      scrollTop: $(destSection).offset().top + scrollOffset
-    }, 2000, 'easeInOutExpo');
-  }
-
-  $('.nav-menu a').bind('click', function(event) {
-    event.preventDefault();
-    var clickedMenu = $(this);
-    $('.nav-menu .active').toggleClass('active');
-    clickedMenu.parent().toggleClass('active');
-    scrollToSection(clickedMenu.attr('href'));
-  });
-
-})(jQuery);
-
-
-
-// Sticky Nav
-$(window).scroll(function(e) {
-  var nav_anchor = $(".top-menu-holder");
-  var gotop = $(document);
-  if ($(this).scrollTop() >= 500) {
-    $('.goto-top').css({'opacity': 1});
-  } else if ($(this).scrollTop() < 500)
-  {
-    $('.goto-top').css({'opacity': 0});
-  }
-  if ($(this).scrollTop() >= $('header').height())
-  {
-    nav_anchor.addClass('split');
-  }
-  else if ($(this).scrollTop() < $('header').height())
-  {
-    nav_anchor.removeClass('split');
-  }
+$(window).load(function () {
+    $('#preloader').delay(350).fadeOut('slow', function () {
+        $('.profile-page, .resume-page, .contact-page').hide();
+    });
 });
 
+$(document).ready(function () {
 
+    'use strict';
 
+    /*  ---------------------
+         Homepage Responsive
+        ---------------------  */
+    
 
-/**
- Provides requestAnimationFrame in a cross browser way.
- @author paulirish / http://paulirish.com/ */
-if (!window.requestAnimationFrame) {
-  window.requestAnimationFrame = (function() {
-    return window.webkitRequestAnimationFrame ||
-            window.mozRequestAnimationFrame ||
-            window.oRequestAnimationFrame ||
-            window.msRequestAnimationFrame ||
-            function(/* function FrameRequestCallback / callback, / DOMElement Element */ element) {};
-  })();
+    function homepageResponsive() {
 
-}
+        // Homepage Main Portions Responsive
+
+        var windowsWidth = $(window).width(),
+            windowsHeight = $(window).height();
+
+        if (windowsWidth > windowsHeight) {
+
+            $('.introduction , .menu').css({
+                width: '50%',
+                height: '100%'
+            });
+
+        } else {
+
+            $('.introduction , .menu').css({
+                width: '100%',
+                height: '50%'
+            });
+
+        }
+
+        // Homepage Profile Image Responsive
+
+        var introWidth = $('.introduction').width(),
+            introHeight = $('.introduction').height(),
+            bgImage = $('.introduction').find('img');
+
+        if (introWidth > introHeight) {
+
+            bgImage.css({
+                width: '100%',
+                height: 'auto'
+            });
+
+        } else {
+
+            bgImage.css({
+                width: 'auto',
+                height: '100%'
+            });
+
+        }
+
+    }
+
+    $(window).on('load resize', homepageResponsive);
+
+    /*  --------------
+         Menu Settings
+        --------------  */
+
+    // Hide Menu
+
+    $('.menu > div').on('click', function () {
+
+        var introWidth = $('.introduction').width(),
+            menuWidth = $('.menu').width();
+
+        $('.introduction').animate({
+            left: '-' + introWidth
+        }, 1000, 'easeOutQuart');
+        $('.menu').animate({
+            left: menuWidth
+        }, 1000, 'easeOutQuart', function () {
+            $('.home-page').css({
+                visibility: 'hidden'
+            });
+        });
+
+    });
+
+    // Show Reletive Page Onclick
+
+    $('.menu div.profile-btn').on('click', function () {
+        $('.profile-page').fadeIn(1200);
+    });
+
+    $('.menu div.resume-btn').on('click', function () {
+        $('.resume-page').fadeIn(1200);
+    });
+
+    $('.menu div.portfolio-btn').on('click', function () {
+        $('.portfolio-page').fadeIn(1200);
+    });
+
+    $('.menu div.contact-btn').on('click', function () {
+        $('.contact-page').fadeIn(1200);
+    });
+
+    // Close Button, Hide Menu
+
+    $('.close-btn').on('click', function () {
+        $('.home-page').css({
+            visibility: 'visible'
+        });
+        $('.introduction, .menu').animate({
+            left: 0
+        }, 1000, 'easeOutQuart');
+        $('.profile-page, .resume-page, .portfolio-page, .contact-page').fadeOut(800);
+    });
+    
+    /*  --------------------------------
+         Maximize Services Items Height
+        --------------------------------  */
+    
+    function maximizeHeight() {
+        
+        var minHeight = 0;
+        
+        $('.services').each(function () {
+            
+            var maxHeight = $(this).height();
+            
+            if (maxHeight > minHeight) {
+                minHeight = maxHeight;
+            }
+            
+        });
+        
+        $('.services').height(minHeight);
+    }
+    
+    maximizeHeight();
+    
+    $(window).on('resize', maximizeHeight);
+
+    /*  ----------------------------------------
+         Tooltip Starter for Social Media Icons
+        ----------------------------------------  */
+
+    $('.intro-content .social-media [data-toggle="tooltip"]').tooltip({
+        placement: 'bottom'
+    });
+
+    $('.contact-details .social-media [data-toggle="tooltip"]').tooltip();
+
+    /*  ------------
+         Pie Charts
+        ------------  */
+
+    $(function () {
+
+        var fillColor = $('.footer').css('background-color'); // Get background color of footer to set relevent fill color in pie charts
+
+        // Initiate EasyPieChart
+
+        $('.skill').easyPieChart({
+            barColor: fillColor,
+            trackColor: '#c6c6c6',
+            scaleColor: '#c6c6c6',
+            scaleLength: 8,
+            lineWidth: 8,
+            size: 150,
+            lineCap: 'butt'
+        });
+
+    });
+
+    /*  -------------------------------
+         MixItUp ( for portfolio page )
+        -------------------------------  */
+
+    $(function () {
+
+        // Initiate MixItUp
+
+        $('#projects').mixItUp({
+            
+            callbacks: {
+                onMixLoad: function (state) {
+                    $('.portfolio-page').hide();
+                }
+            }
+            
+        });
+
+    });
+
+    /*  -------------------------------
+         PopUp ( for portfolio page )
+        -------------------------------  */
+
+    $(function () {
+        $('.show-popup').popup({
+            keepInlineChanges: true,
+            speed: 500
+        });
+    });
+
+    /*  -----------------------------------------------------
+         MixItUp ( for portfolio page : testimonials slider )
+        -----------------------------------------------------  */
+
+    $(function () {
+        $(".owl-carousel").owlCarousel({
+            navigation: false, // Show next and prev buttons
+            slideSpeed: 200,
+            paginationSpeed: 300,
+            singleItem: true
+        });
+    });
+    
+    /*  -------------
+         Contact Form
+        ------------- */
+
+    $('#contactForm').submit(function () {
+        
+        $.ajax({
+            type: "POST",
+            url: "php/contact.php",
+            data: $('#contactForm').serialize(),
+            success: function (msg) {
+                if (msg == 'SEND') {
+                    $('.success').fadeIn();
+                    $('.error').fadeOut();
+                    $('#contactForm')[0].reset();
+                } else {
+                    $('.success').fadeOut();
+                    $('.error').fadeIn().find('h3').text(msg);
+                }
+            }
+        });
+        return false;
+    });
+
+    /*  -------------------------------
+         Google Map ( for contact page )
+        -------------------------------  */
+
+    $('#google-map').gMap({
+        latitude: 31.562130,
+        longitude: 74.319460,
+        maptype: 'TERRAIN',
+        scrollwheel: false,
+        zoom: 14,
+        markers: [
+            {
+                latitude: 31.562130,
+                longitude: 74.319460,
+                html: "I am Here!",
+                icon: {
+                    image: "images/icon/map_marker.png",
+                    iconsize: [46, 46],
+                    iconanchor: [12, 46]
+                }
+            }
+        ],
+        controls: {
+            panControl: false,
+            zoomControl: true,
+            mapTypeControl: false,
+            scaleControl: false,
+            streetViewControl: false,
+            overviewMapControl: false
+        }
+    });
+
+});
